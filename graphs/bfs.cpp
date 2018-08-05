ce162b86612e61d056953b941ca62c0c822e9775
Aritra Sur Roy <aritra1997@hotmail.com>
2018-08-05 12:42:42 +0530

    Added Recursive BFS

--- graphs/bfs.cpp
+++ graphs/bfs.cpp
@@ -10,6 +10,8 @@ using namespace std;
 class graph{
 	int V;				// Total no. of vertices
 	list<int> *adj;		// Pointer to the array of adjacency lists of the graph
+	
+	void doBfsUtil(queue<int>& q, bool* visited);	// utility function for bfs recursive
 public:
 	graph(int V){
 		this->V = V;
@@ -19,14 +21,52 @@ public:
 		delete [] adj;
 	}
 	void addEdge(int u,int v);	// adds an edge between u and v
-	void bfs(int s);			// do a breadth first search on the graph from s
+	void bfs_iterative(int s);			// do a breadth first search on the graph from s
+	void bfs_recursive(int s);			// recursive implementation of bfs
 };
 
 void graph::addEdge(int u,int v){
 	adj[u].push_back(v);
 }
 
-void graph::bfs(int s){
+void graph::doBfsUtil(queue<int>& q, bool* visited){
+	// if I don't have an empty queue of vertices to visit
+	if(!q.empty()){
+		int u = q.front();	// get the first vertex
+		q.pop();
+		
+		if(!visited[u]){	// if it hasn't been visited
+			cout << u << " ";	// then visit it
+			visited[u] = true;
+		}
+		
+		// now for all adjacent vertex to u 
+		for(auto it = adj[u].begin(); it != adj[u].end(); it++){
+			// only if it hasn't been visited
+			if(!visited[*it])
+				q.push(*it);	// push it to the queue
+		}
+		
+		doBfsUtil(q, visited);	// recurse for the next added vertex
+	}else{
+		return;
+	}
+	
+}
+void graph::bfs_recursive(int s){
+	bool *visited = new bool[V];	// create an array to store which vertices are visited
+	queue<int> q;		// queue for bfs
+	
+	for(int i = 0;i < V;i++){
+		visited[i] = false;
+	}
+	
+	q.push(s);				// push the starting node
+	
+	doBfsUtil(q, visited);	
+}
+
+void graph::bfs_iterative(int s){
 	bool *visited = new bool[V];	// create an array to store which vertices are visited
 	queue<int> q;		// queue for bfs
 	list<int>::iterator it;	// Adjacency list iterator
@@ -66,7 +106,13 @@ int main(){
     g.addEdge(2, 3);
     g.addEdge(3, 3);
 	
-	g.bfs(2);
+	cout << "Breadth First Search Iterative:" << endl;
+	g.bfs_iterative(2);
+	cout << endl;
+	
+	cout << "Breadth First Search Recursive:" << endl;
+	g.bfs_recursive(2);
+	cout << endl;
 	
 	return 0;
 }
